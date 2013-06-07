4664a801f6afa6751d3dcc273d0f44130378c0c8
Brian Heithaus <bheithaus@gmail.com>
2013-06-06 21:17:41 -0700

    fix .html_safe called on Nil bug

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,9 +2,9 @@ class StaticPagesController < ApplicationController
   include RedisHelper
   
   def app
-    @radio_stations_json = public_stations
+    @radio_stations_json = public_stations.html_safe
     if current_user
-      @current_user_json = current_user_and_favorite_tracks #in AppCtrl
+      @current_user_json = current_user_and_favorite_tracks.html_safe #in AppCtrl
     end
         
     respond_to do |format|
