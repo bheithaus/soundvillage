c22cc5088f0c2ba7741b95d770204732ed3f4866
Brian Heithaus <bheithaus@gmail.com>
2013-06-14 15:08:32 -0700

    p params, root request

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,6 +2,7 @@ class StaticPagesController < ApplicationController
   include RedisHelper
   
   def app
+    p params
     @radio_stations_json = public_stations.html_safe
     @current_user_json = current_user ? 
                           current_user_and_favorite_tracks.html_safe : nil.to_json
