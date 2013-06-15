6ce73971bcfb465180ae56ec153f79edc294153e
Brian Heithaus <bheithaus@gmail.com>
2013-06-15 10:51:17 -0700

    set redirect uri explicit on facebook omniauth

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -216,6 +216,7 @@ Devise.setup do |config|
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', :scope => 'user,public_repo'
   config.omniauth :facebook, ENV["FACEBOOK_APP_ID"], ENV["FACEBOOK_SECRET"],
                   scope: 'email, publish_actions',
+                  redirect_uri: 'http://soundvillage.herokuapp.com/users/auth/facebook/callback',
                   display: :popup
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
