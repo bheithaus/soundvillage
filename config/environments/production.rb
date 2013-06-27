aa2ab6c58856a5312996a59990004abdb322e3e4
Brian Heithaus <bheithaus@gmail.com>
2013-06-26 17:22:01 -0700

    update asset host with region s3-us-west-1

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 Soundvillage::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
   # AMAZON S3 CDN
-  config.action_controller.asset_host = "https://#{ENV['FOG_DIRECTORY']}.s3.amazonaws.com"
+  config.action_controller.asset_host = "https://#{ENV['FOG_DIRECTORY']}.s3-us-west-1.amazonaws.com"
 
   # Code is not reloaded between requests
   config.cache_classes = true
